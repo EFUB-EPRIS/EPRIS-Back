aa767f02e17a90213f1f24555e912ba098749e1d
SuJi Mo <ahtnwl1004@gmail.com>
2024-08-06 03:41:28 +0900

    fix : pesigned url 이용 이미지 업로드 기능 개발 오류 수정

--- src/main/java/com/epris/homepage/global/config/S3Config.java
+++ src/main/java/com/epris/homepage/global/config/S3Config.java
@@ -5,33 +5,50 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.accessKey}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
-                .build();
+    @Primary
+    public BasicAWSCredentials awsCredentialsProvider(){
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return basicAWSCredentials;
     }
 
+    @Bean
+    public AmazonS3 amazonS3() {
+        AmazonS3 s3Builder = AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+                .build();
+        return s3Builder;
+    }
+
+    @Bean
+    public S3Presigner presigner() {
+        return S3Presigner.builder()
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
 
 }
--- src/main/java/com/epris/homepage/global/service/FileService.java
+++ src/main/java/com/epris/homepage/global/service/FileService.java
@@ -2,36 +2,50 @@ package com.epris.homepage.global.service;
 
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.RequestPayer;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.RequestPayer;
 
 import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Component
 public class FileService {
+
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
     private final AmazonS3 amazonS3;
 
+    private final AmazonS3Client amazonS3Client;
+
     /**
      * presigned url 발급
-     * @param prefix 버킷 디렉토리 이름
-     * @param fileName 클라이언트가 전달한 파일명 파라미터
-     * @return presigned url
      */
-    public String getPreSignedUrl(String prefix, String fileName) {
-        if(!prefix.equals("")) {
-            fileName = createPath(prefix, fileName);
-        }
-
+    public String getPreSignedUrl(String fileName) throws Exception {
         GeneratePresignedUrlRequest generatePresignedUrlRequest = getGeneratePreSignedUrlRequest(bucket, fileName);
         URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
         return url.toString();
@@ -39,24 +53,26 @@ public class FileService {
 
     /**
      * 파일 업로드용(PUT) presigned url 생성
-     * @param bucket 버킷 이름
-     * @param fileName S3 업로드용 파일 이름
-     * @return presigned url
      */
-    private GeneratePresignedUrlRequest getGeneratePreSignedUrlRequest(String bucket, String fileName) {
+    private GeneratePresignedUrlRequest getGeneratePreSignedUrlRequest(String bucket, String fileName) throws Exception {
+
+        //if(fileName.equals(null)) throw new Exception(new Exception("널이 들어왔습니다"));
         GeneratePresignedUrlRequest generatePresignedUrlRequest =
                 new GeneratePresignedUrlRequest(bucket, fileName)
                         .withMethod(HttpMethod.PUT)
                         .withExpiration(getPreSignedUrlExpiration());
         generatePresignedUrlRequest.addRequestParameter(
                 Headers.S3_CANNED_ACL,
+                //CannedAccessControlList.BucketOwnerFullControl.toString());
                 CannedAccessControlList.PublicRead.toString());
+        //generatePresignedUrlRequest.addRequestParameter("x-amz-request-payer", "bucket-owner");
+
+
         return generatePresignedUrlRequest;
     }
 
     /**
      * presigned url 유효 기간 설정
-     * @return 유효기간
      */
     private Date getPreSignedUrlExpiration() {
         Date expiration = new Date();
@@ -68,7 +84,6 @@ public class FileService {
 
     /**
      * 파일 고유 ID를 생성
-     * @return 36자리의 UUID
      */
     private String createFileId() {
         return UUID.randomUUID().toString();
@@ -76,11 +91,28 @@ public class FileService {
 
     /**
      * 파일의 전체 경로를 생성
-     * @param prefix 디렉토리 경로
-     * @return 파일의 전체 경로
      */
-    private String createPath(String prefix, String fileName) {
+    private String createPath(String fileName) {
         String fileId = createFileId();
-        return String.format("%s/%s", prefix, fileId + fileName);
+        return String.format("%s/%s", fileId + fileName);
+    }
+
+    /* 파일 유효성 검사*/
+    private String getFileExtension(String fileName) {
+        if (fileName.length() == 0) {
+            //throw new PrivateException(Code.WRONG_INPUT_IMAGE);
+        }
+        ArrayList<String> fileValidate = new ArrayList<>();
+        fileValidate.add(".jpg");
+        fileValidate.add(".jpeg");
+        fileValidate.add(".png");
+        fileValidate.add(".JPG");
+        fileValidate.add(".JPEG");
+        fileValidate.add(".PNG");
+        String idxFileName = fileName.substring(fileName.lastIndexOf("."));
+        if (!fileValidate.contains(idxFileName)) {
+            //throw new PrivateException(Code.WRONG_IMAGE_FORMAT);
+        }
+        return fileName.substring(fileName.lastIndexOf("."));
     }
 }
