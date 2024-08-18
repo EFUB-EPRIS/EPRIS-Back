c0f7a4d48fc5f92787498b5aaf5d069839acbd44
SuJi Mo <ahtnwl1004@gmail.com>
2024-08-18 22:46:03 +0900

    refactor : 네트워크 수정 시 기존 이미지를 S3에서 삭제

--- src/main/java/com/epris/homepage/activity/network/controller/NetworkController.java
+++ src/main/java/com/epris/homepage/activity/network/controller/NetworkController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,7 @@ public class NetworkController {
     private final NetworkService networkService;
 
     @PostMapping
-    public ResponseEntity<NetworkResponseDto> updateNetwork(@RequestParam("type")String type, @RequestBody NetworkReqeustDto reqeustDto){
+    public ResponseEntity<NetworkResponseDto> updateNetwork(@RequestParam("type")String type, @RequestBody NetworkReqeustDto reqeustDto) throws IOException {
         return networkService.updateNetwork(type,reqeustDto);
     }
 
