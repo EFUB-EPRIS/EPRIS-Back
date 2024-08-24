31eb01844f57b62c41fa7bc856259650008d2a10
SuJi Mo <ahtnwl1004@gmail.com>
2024-08-25 00:42:13 +0900

    exception : @Valid 유효성 체크 예외 처리

--- src/main/java/com/epris/homepage/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/epris/homepage/global/exception/ApiExceptionHandler.java
@@ -2,8 +2,10 @@ package com.epris.homepage.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @Slf4j
 @RestControllerAdvice
@@ -12,5 +14,12 @@ public class ApiExceptionHandler {
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         log.error("[handleCustomException] {} : {}",e.getErrorCode().name(), e.getErrorCode().getMessage());
         return ErrorResponse.error(e);
+    }
+
+    /* @Valid 유효성 체크 */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodValidException(MethodArgumentNotValidException e){
+        log.warn("MethodArgumentNotValidException 발생!!!  trace:{}", e.getStackTrace());
+        return ErrorResponse.error(new CustomException(ErrorCode.INPUT_IS_NULL));
     }
 }
