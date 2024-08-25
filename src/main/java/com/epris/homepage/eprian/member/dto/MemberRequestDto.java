aeab2eb4d1c4633446acaea13365ac265c4d1244
SuJi Mo <ahtnwl1004@gmail.com>
2024-08-25 15:51:18 +0900

    refactor : 요청 파라미터의 validation 체크

--- src/main/java/com/epris/homepage/eprian/member/dto/MemberRequestDto.java
+++ src/main/java/com/epris/homepage/eprian/member/dto/MemberRequestDto.java
@@ -1,18 +1,35 @@
 package com.epris.homepage.eprian.member.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class MemberRequestDto {
+
+    @NotBlank
     private Boolean isNew;
+
+    @NotBlank
     private String num;
+
+    @NotBlank
     private Long memberId;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String position;
+
+    @NotBlank
     private String memberInfo;
+
+    @NotBlank
     private String profileUrl;
+
+    @NotBlank
     private Boolean isActive;
 }
