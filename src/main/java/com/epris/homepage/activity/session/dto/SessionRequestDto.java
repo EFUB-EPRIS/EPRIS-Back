c1a8fb0e2a712e010acba3279811ee9a68205798
SuJi Mo <ahtnwl1004@gmail.com>
2024-08-25 15:55:42 +0900

    refactor : 요청 파라미터의 validation 체크

--- src/main/java/com/epris/homepage/activity/session/dto/SessionRequestDto.java
+++ src/main/java/com/epris/homepage/activity/session/dto/SessionRequestDto.java
@@ -1,6 +1,7 @@
 package com.epris.homepage.activity.session.dto;
 
 import com.epris.homepage.global.dto.ImageUrl;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,10 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class SessionRequestDto {
+
+    @NotBlank
     private String sessionInfo;
+
+    @NotBlank
     private List<ImageUrl> imageUrlList;
 }
